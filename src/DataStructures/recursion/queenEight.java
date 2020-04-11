271766889c844f98849d9ce8bf6d25b3c7d83e07
YichangKong <50892995+YichangKong@users.noreply.github.com>
2020-04-11 15:17:16 +0800

    完成queenEight 问题的注释修改

--- src/DataStructures/recursion/queenEight.java
+++ src/DataStructures/recursion/queenEight.java
@@ -1,68 +1,89 @@
 package DataStructures.recursion;
 
 /**
- * @author yichangkong
- * @create 2020-04-10-19:50
- */
+* @author yichangkong
+* @create 2020-04-10-19:50
+*/
 
 public class queenEight {
 
-    static int[] queen = new int[8];
-    static int count;
-
+    //定义一个max表示共有多少个皇后
+    int max = 8;
+    //定义数组array, 保存皇后放置位置的结果,比如 arr = {0 , 4, 7, 5, 2, 6, 1, 3}
+    int[] array = new int[max];
+    static int count = 0;
+    static int judgeCount = 0;
     public static void main(String[] args) {
-        queenEight queenEight = new queenEight();
-        queenEight.check(0);
-        System.out.println("解决路径总数： " + count);
+        //测试 8皇后是否正确
+        queenEight queue8 = new queenEight();
+        queue8.check(0);
+        System.out.printf("一共有%d解法", count);
+        System.out.printf("一共判断冲突的次数%d次", judgeCount); // 1.5w
+
     }
 
 
+    /**
+     *@Description编写一个方法，放置第n个皇后
+     * 特别注意： check 是 每一次递归时，进入到check中都有  for(int i = 0; i < max; i++)，因此会有回溯
+     *@Param [n]
+     *@Return void
+     *@Date 2020/4/11
+     *@Time 15:14
+     */
     private void check(int n) {
-        if (n == 8) {
-            count++;
+
+        if(n == max) {  //n = 8 , 其实8个皇后就既然放好
             print();
             return;
         }
 
-        for (int i = 0; i < 8; i++) {
-
-            queen[n] = i;
-
-            if (judgment(n)) {
-
-                check(n + 1);
+        //依次放入皇后，并判断是否冲突
+        for(int i = 0; i < max; i++) {
+            //先把当前这个皇后 n , 放到该行的第1列
+            array[n] = i;
+            //判断当放置第n个皇后到i列时，是否冲突
+            if(judge(n)) { // 不冲突
+                //接着放n+1个皇后,即开始递归
+                check(n+1);
             }
-
+            //如果冲突，就继续执行 array[n] = i; 即将第n个皇后，放置在本行得 后移的一个位置
         }
     }
 
-    public void print() {
-        for (int i = 0; i < queen.length; i++) {
 
-            System.out.print(queen[i] + "   ");
+    /**
+     *@Description   说明
+     *             1. array[i] == array[n]  表示判断 第n个皇后是否和前面的n-1个皇后在同一列
+     *             2. Math.abs(n-i) == Math.abs(array[n] - array[i]) 表示判断第n个皇后是否和第i皇后是否在同一斜线
+     *              n = 1  放置第 2列 1 n = 1 array[1] = 1
+     *              Math.abs(1-0) == 1  Math.abs(array[n] - array[i]) = Math.abs(1-0) = 1
+     *             3. 判断是否在同一行, 没有必要，n 每次都在递增
+     *@Param [n]表示第n个皇后 查看当我们放置第n个皇后, 就去检测该皇后是否和前面已经摆放的皇后冲突
+     *@Return boolean
+     *@Date 2020/4/11
+     *@Time 15:15
+     */
+    private boolean judge(int n) {
 
-        }
-        System.out.println();
-    }
+        judgeCount++;
+        for(int i = 0; i < n; i++) {
 
-    public boolean judgment(int n) {
-
-
-/**
- *@Description
- * queen[i] == queen[n]
- * Math.abs(n - i) == Math.abs(queen[n] - queen[i]
- *@Param [n]
- *@Return boolean
- *@Date 2020/4/10
- *@Time 21:31
- */
-        for (int i = 0; i < n; i++) {
-
-            if (queen[i] == queen[n] || Math.abs(n - i) == Math.abs(queen[n] - queen[i])) {
+            if(array[i] == array[n] || Math.abs(n-i) == Math.abs(array[n] - array[i]) ) {
                 return false;
             }
         }
         return true;
     }
+
+
+    private void print() {//写一个方法，可以将皇后摆放的位置输出
+        count++;
+        for (int i = 0; i < array.length; i++) {
+            System.out.print(array[i] + " ");
+        }
+        System.out.println();
+    }
+
 }
+
