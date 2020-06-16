54310238273da7b16f5ea2c3782e5e2515bef74f
YichangKong <50892995+YichangKong@users.noreply.github.com>
2020-06-17 00:36:54 +0800

    线程Sleep、Yield、Jion

---
+++ src/com/yichangkong/test.java
@@ -0,0 +1,22 @@
+package com.yichangkong;
+
+/**
+ * @author yichangkong
+ * @create 2020-05-31-13:43
+ */
+public class test {
+  public static void main(String[] args) {
+    //
+      final boolean flag=true;
+     while (flag){
+          if (false){
+        System.out.println("false");
+          }else {
+              continue;
+          }
+
+      }
+
+
+  }
+}
